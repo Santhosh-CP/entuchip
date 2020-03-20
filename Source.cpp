d769a81e6b986f99f58a74f5b7008b4ac0318b0a
ShupperMuppet <pok.santhosh@gmail.com>
2020-03-20 12:18:41 +0530

    Chip8 graphics being displayed with minor bugs

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
-#include "Entu.h"
+#include<string>
+#include "entu.h"
 #include "GL/freeglut.h"
 
 using namespace std;
 
-//Window dimensions
-#define WINDOW_HEIGHT 512
-#define WINDOW_WIDTH 1024
-
 //Original Chip-8 Screen dimensions
-#define SCREEN_HEIGHT 32
 #define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+#define OFFSET 12
+
+//Window dimensions
+#define WINDOW_WIDTH SCREEN_WIDTH * OFFSET
+#define WINDOW_HEIGHT SCREEN_HEIGHT * OFFSET
+
+
+
+
 unsigned char ScreenGraphics[SCREEN_WIDTH][SCREEN_WIDTH][3]; //For RGB
 
 Entu entuchip;
@@ -20,6 +26,8 @@ Entu entuchip;
 /*
 TASKS
 -----
+Refactor code
+Use function prototypes in source.cpp
 chip8 display and key functions need to be implemented
 Load ROM function needs to be implemented
 The rom to be loaded should be selected by File -> Load ROM in the GUI
@@ -51,113 +59,113 @@ A Stack is needed to handle jumps. This stack has 16 levels
 A Hexbased keypad
 */
 
+
+
+//Func
+void display();
+void reshape(int w, int h);
 void keyboardPressed(unsigned char key, int x, int y);
 void keyboardUnpressed(unsigned char key, int x, int y);
 
-void createTextures() {
+//Graphics
+void drawScreenPixel(int x, int y);
 
-	//Clear the screen
-	for (int i = 0; i < SCREEN_HEIGHT; i++) {
-		for (int j = 0; j < SCREEN_WIDTH; j++) {
-			ScreenGraphics[i][j][0] = 0;
-			ScreenGraphics[i][j][1] = 0;
-			ScreenGraphics[i][j][2] = 0;
-		}
-	}
+//Method 2
+void createTextures();
+void updateTextures(const Entu entu);
 
-	//Creating a texture
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)ScreenGraphics);
 
-	//Setting it up
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
-	//Enabling the texture
-	glEnable(GL_TEXTURE_2D);
+
+
+
+int main(int argc, char** argv) {
+	cout << "This is Entu Chip!" << endl;
+
+	//Debug Code
+	string rom_name = "C:\\Users\\santh\\source\\repos\\EntuChip\\x64\\Debug\\invaders.c8";
+
+
+	if (!entuchip.load(rom_name))
+		return 1;
+
+
+	//Display related code
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowPosition(512, 512);
+	glutCreateWindow("Entu Chip by Santhosh C P");
+
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboardPressed);
+	glutKeyboardFunc(keyboardUnpressed);
+
+
+	
+	//createTextures();
+	glutMainLoop();
+	
+
+	return 0;
 }
 
-void updateTextures(const Entu entu) {
-	int pixel;
 
-	//Updating the screen
-	for (int i = 0; i < SCREEN_HEIGHT; i++) {
-		for (int j = 0; j < SCREEN_WIDTH; j++) {
-			pixel = i * 64 + j;
+void drawScreenPixel(int x, int y) {
+	//Converting to GLfloat values
+	GLfloat X = x  + 0.0f;
+	GLfloat Y = y  + 0.0f;
 
-			if (entu.Graphics[pixel] == 0) {
-				ScreenGraphics[i][j][0] = 0;
-				ScreenGraphics[i][j][1] = 0;
-				ScreenGraphics[i][j][2] = 0;
+	glBegin(GL_QUADS);
+	glVertex3f(X, Y, 0);
+	glVertex3f(X, Y + 1, 0);
+	glVertex3f(X + 1, Y + 1, 0);
+	glVertex3f(X + 1, Y, 0);
+}
+
+void updateQuads(const Entu entu) {
+
+	for (int y = 0; y < SCREEN_HEIGHT; y++) {
+		for (int x = 0; x < SCREEN_WIDTH; x++) {
+			if (entu.Graphics[(y * 64) + x] == 0) {
+				glColor3f(0.0f, 0.0f, 0.0f);
 			}
 			else {
-				ScreenGraphics[i][j][0] = 255;
-				ScreenGraphics[i][j][1] = 255;
-				ScreenGraphics[i][j][2] = 255;
+				glColor3f(1.0f, 1.0f, 1.0f);
 			}
+
+			drawScreenPixel(x, y);
 		}
 	}
-
-	//Updating the textures
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)ScreenGraphics);
-
-
-	//Why?
-	glBegin(GL_QUADS);
-
-	glTexCoord2d(0.0, 0.0);
-	glVertex2d(0.0, 0.0);
-
-	glTexCoord2d(1.0, 0.0);
-	glVertex2d(WINDOW_WIDTH, 0.0);
-
-	glTexCoord2d(1.0, 1.0);
-	glVertex2d(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-	glTexCoord2d(0.0, 1.0);
-	glVertex2d(0.0, WINDOW_HEIGHT);
-
-	glBegin(GL_QUADS);
 }
 
-void myDisplay() {
+void display() {
 	entuchip.cycle();
 
 	if (entuchip.drawFlag) {
 		glClear(GL_COLOR_BUFFER_BIT); //Clears the color buffer(background)
-		updateTextures(entuchip);
+
+
+		//updateTextures(entuchip);
+		updateQuads(entuchip);
+
 		glutSwapBuffers();
 		entuchip.drawFlag = false;
 	}
 }
 
-int main(int argc, char** argv) {
-	cout << "This is Entu Chip!" << endl;
+void reshape(int w, int h) {
+	glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
 
-	if (!entuchip.load(argv[1]))
-		return 1;
-
-	//Display related code
-	glutInit(&argc, argv);
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Entu Chip by Santhosh C P");
-	glutDisplayFunc(myDisplay);
-	glutIdleFunc(myDisplay);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 32, 0, GL_RGBA, GL_UNSIGNED_BYTE, entuchip.Graphics);
-	glutMainLoop();
-	
-	glutKeyboardFunc(keyboardPressed);
-	glutKeyboardFunc(keyboardUnpressed);
-	//TODO - Load ROM here
-
-
-	while (true) {
-		entuchip.cycle();
-	}
-
-	return 0;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, SCREEN_WIDTH, SCREEN_HEIGHT,0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, w, h);
 }
+
 
 void keyboardPressed(unsigned char key, int x, int y) {
 	// We will be using the first 4 keys on the 4 rows of the keyboard
@@ -244,3 +252,75 @@ void keyboardUnpressed(unsigned char key, int x, int y) {
 	else if (key == 'v')
 		entuchip.Key[0xF] = 0;
 }
+
+
+//Not needed. Delete once the output is proper
+void createTextures() {
+
+	//Clear the screen
+	for (int i = 0; i < SCREEN_HEIGHT; i++) {
+		for (int j = 0; j < SCREEN_WIDTH; j++) {
+			ScreenGraphics[i][j][0] = 0;
+			ScreenGraphics[i][j][1] = 0;
+			ScreenGraphics[i][j][2] = 0;
+		}
+	}
+
+	//Creating a texture
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)ScreenGraphics);
+
+	//Setting it up
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	//Enabling the texture
+	glEnable(GL_TEXTURE_2D);
+}
+void updateTextures(const Entu entu) {
+	int pixel;
+
+	//Updating the screen
+	for (int i = 0; i < SCREEN_HEIGHT; i++) {
+		for (int j = 0; j < SCREEN_WIDTH; j++) {
+			pixel = i * 64 + j;
+
+			if (entu.Graphics[pixel] == 0) {
+				ScreenGraphics[i][j][0] = 0;
+				ScreenGraphics[i][j][1] = 0;
+				ScreenGraphics[i][j][2] = 0;
+			}
+			else {
+				ScreenGraphics[i][j][0] = 255;
+				ScreenGraphics[i][j][1] = 255;
+				ScreenGraphics[i][j][2] = 255;
+			}
+		}
+	}
+
+	//Updating the textures
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)ScreenGraphics);
+
+
+	//Why?
+	glBegin(GL_QUADS);
+
+	glTexCoord2d(0.0, 0.0);
+	glVertex2d(0.0, 0.0);
+
+	glTexCoord2d(1.0, 0.0);
+	glVertex2d(WINDOW_WIDTH, 0.0);
+
+	glTexCoord2d(1.0, 1.0);
+	glVertex2d(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	glTexCoord2d(0.0, 1.0);
+	glVertex2d(0.0, WINDOW_HEIGHT);
+
+	glEnd();
+}
+
+
+
+
---
+++ entu.cpp
@@ -0,0 +1,637 @@
+#include "entu.h"
+
+Entu::Entu() {
+	// We aren't doing anything using the constructor
+}
+
+Entu::~Entu() {
+	// We aren't doing anything using the destructor
+}
+
+
+void Entu::initialize() {
+
+
+	//resetting the registers
+	PC = 0x200; //PC starts at 0x200
+	Opcode = 0;
+	I = 0;
+	StackPointer = 0;
+
+	//Filling all the arrays with 0 in all memory locations. Essentially "reset"ting them
+	fill(begin(Memory), end(Memory), 0);
+	fill(begin(v), end(v), 0);
+	fill(begin(Graphics), end(Graphics), 0);
+	fill(begin(Stack), end(Stack), 0);
+	fill(begin(Key), end(Key), 0);
+
+	//resetting the timers
+	DelayTimer = 0;
+	SoundTimer = 0;
+
+	//Loading the fontset
+	//The fontset is stored from the memory address 0x50
+	for (int i = 0; i < 80; i++) {
+		Memory[i] = FontSet[i];
+	}
+
+	//Clear the screen once the initialisation is complete
+	drawFlag = true;
+}
+
+bool Entu::load(string filename) {
+	initialize();
+
+	const char* _FileName = filename.c_str();
+	FILE* file = fopen(_FileName, "rb");
+	if (file == NULL) {
+		printf("ROM file does not exist or incorrect filename\n");
+		return false;
+	}
+
+	long romSize;
+
+	fseek(file, 0, SEEK_END);
+	romSize = ftell(file);
+	rewind(file);
+
+	char* romData = (char*)malloc(sizeof(char) * romSize);
+	if (romData == NULL) {
+		printf("Failure to load the rom data\n");
+		return false;
+	}
+
+	size_t rom = fread(romData, sizeof(char), (size_t)romSize, file);
+
+	int offset = 512;
+	int max = 4096;
+
+	if ((max - offset) > romSize) {
+		//Rom can be loaded
+
+		for (int i = 0; i < romSize; i++) {
+			Memory[offset + i] = (unsigned char)romData[i];
+		}
+	}
+	else {
+		printf("ROM size is larger than the available memory. Unable to load the ROM file");
+		return false;
+	}
+
+	fclose(file);
+	free(romData);
+
+	return true;
+}
+
+void Entu::cycle() {
+	//Obtaining the opcode
+	Opcode = getOpcode(Memory[PC], Memory[PC + 1]);
+
+
+	//Debug Test Code
+	//Opcode = 0xFD1E;
+	//unsigned short test =  0xFD1E & 0xF000;
+	//cout << test << endl;
+
+
+	//Decoding the opcode
+	switch (Opcode & 0xF000) {
+
+		/*
+		Reason for the switch value "Opcode & 0xF000"
+
+		Chip 8 Opcodes can be divided into 16 subdivisions based on the value of the first 4 bits (0xN000)
+		Futher subdivision is done for certain Opcodes which have multiple Opcodes having the same 4 first bits
+		So when we perform the Bitwise AND operation on the Opcode, we eliminate the last 12 bits and keep only the first 4 bits
+		The comparison is done based on the first 4 bits
+		*/
+		//verified
+	case 0x0000: {
+		unsigned short last;
+
+		last = Opcode & 0x00FF;
+		if (last == 0x00E0) {
+			//00E0 - cls
+			//Clears the screen
+			fill(begin(Graphics), end(Graphics), 0);
+			drawFlag = true;
+			PC = PC + 2;
+		}
+		else if (last == 0x00EE) {
+			//00EE - RET
+			//Return from a subroutine
+			StackPointer--;
+			PC = Stack[StackPointer];
+			PC = PC + 2;
+		}
+		else {
+			cout << "\nUnsupported 0x0000 series Opcode\n";
+		}
+		break;
+	}
+
+
+			   //verified
+	case 0x1000: {
+		//1nnn - JP addr
+		//Jump to location nnn.
+		PC = Opcode & 0x0FFF;
+		break;
+	}
+			   //verified
+	case 0x2000: {
+		//2nnn - CALL addr
+		//Call subroutine at nnn.
+		Stack[StackPointer] = PC;
+		StackPointer++;
+		PC = Opcode & 0x0FFF;
+		break;
+	}
+			   //verified
+	case 0x3000: {
+		//3xkk - SE Vx, byte
+		//Skip next instruction if Vx = kk.
+		unsigned short x, kk;
+		x = getX();
+		kk = getKK();
+		if (v[x] == kk) {
+			PC = PC + 2;
+		}
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0x4000: {
+		//4xkk - SNE Vx, byte
+		//Skip next instruction if Vx != kk.
+		unsigned short x, kk;
+		x = getX();
+		kk = getKK();
+		if (v[x] != kk) {
+			PC = PC + 2;
+		}
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0x5000: {
+		//5xy0 - SE Vx, Vy
+		//Skip next instruction if Vx = Vy.
+		unsigned short x, y;
+		x = getX();
+		y = getY();
+		if (v[x] == v[y]) {
+			PC = PC + 2;
+		}
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0x6000: {
+		//6xkk - LD Vx, byte
+		//Set Vx = kk.
+		unsigned char x, kk;
+		x = getX();
+		kk = getKK();
+		v[x] = kk;
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0x7000: {
+		//7xkk - ADD Vx, byte
+		//Set Vx = Vx + kk.
+		unsigned short x, kk;
+		x = getX();
+		kk = getKK();
+		v[x] = v[x] + kk;
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0x8000: {
+		//There are multiple opcodes which start with 8
+		//They are differentiated based on the last 4 bits
+		//All commands of this type have X & Y values
+		unsigned short x, y, last;
+		last = Opcode & 0x000F;
+		x = getX();
+		y = getY();
+		switch (last) {
+			//8xy0 - LD Vx, Vy
+			//Set Vx = Vy.
+		case 0: {
+			v[x] = v[y];
+			break;
+		}
+
+			  //8xy1 - OR Vx, Vy
+			  //Set Vx = Vx OR Vy.
+		case 1: {
+			v[x] = v[x] | v[y];
+			break;
+		}
+
+			  //8xy2 - AND Vx, Vy
+			  //Set Vx = Vx AND Vy.
+		case 2: {
+			v[x] = v[x] & v[y];
+			break;
+		}
+
+			  //8xy3 - XOR Vx, Vy
+			  //Set Vx = Vx XOR Vy.
+		case 3: {
+			v[x] = v[x] ^ v[y];
+			break;
+		}
+
+			  //8xy4 - ADD Vx, Vy
+			  //Set Vx = Vx + Vy, set VF = carry.
+		case 4: {
+			unsigned char sum;
+			sum = v[x] + v[y];
+			if (sum > 255) {
+				v[0xF] = 1;
+				sum = sum & 0x00FF;
+				v[x] = sum;
+			}
+			else {
+				v[0xF] = 0;
+				v[x] = sum;
+			}
+			break;
+		}
+
+			  //8xy5 - SUB Vx, Vy
+				//Set Vx = Vx - Vy, set VF = NOT borrow.
+		case 5: {
+			if (v[x] > v[y]) {
+				v[0xF] = 1;
+			}
+			else {
+				v[0xF] = 0;
+			}
+			v[x] = v[x] - v[y];
+			break;
+		}
+
+			  //8xy6 - SHR Vx {, Vy}
+			  //Set Vx = Vx SHR 1.
+		case 6: {
+			unsigned char end;
+			end = v[x] & 0x0F;
+			end = end & 0b0001;
+			if (end == 1) {
+				v[0xF] = 1;
+			}
+			else {
+				v[0xF] = 0;
+			}
+			v[x] = v[x] >> 1;
+			break;
+		}
+
+			  //8xy7 - SUBN Vx, Vy
+			  //Set Vx = Vy - Vx, set VF = NOT borrow.
+		case 7: {
+			if (v[y] > v[x]) {
+				v[0xF] = 1;
+			}
+			else {
+				v[0xF] = 0;
+			}
+			v[x] = v[y] - v[x];
+			break;
+		}
+
+			  //8xyE - SHL Vx {, Vy}
+			  //Set Vx = Vx SHL 1.
+		case 0xE: {
+			unsigned char start;
+			start = v[x] & 0xF0;
+			start = start >> 7; // Only the MSB is remaining
+			if (start == 1) {
+				v[0xF] = 1;
+			}
+			else {
+				v[0xF] = 0;
+			}
+			v[x] = v[x] << 1;
+			break;
+		}
+
+		default: {
+			cout << "\nUnsupported 0x8000 series Opcode\n";
+			break;
+		}
+		}
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0x9000: {
+		//9xy0 - SNE Vx, Vy
+		//Skip next instruction if Vx != Vy.
+		unsigned short x, y;
+		x = getX();
+		y = getY();
+		if (v[x] != v[y]) {
+			PC = PC + 2;
+		}
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0xA000: {
+		//Annn - LD I, addr
+		//Set I = nnn.
+		I = Opcode & 0x0FFF;
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0xB000: {
+		//Bnnn - JP V0, addr
+		//Jump to location nnn + V0.
+		PC = (Opcode & 0x0FFF) + v[0];
+		break;
+	}
+			   //verified
+	case 0xC000: {
+		//Cxkk - RND Vx, byte
+		//Set Vx = random byte AND kk.
+		unsigned char max, rand_byte;
+		unsigned short x, kk;
+
+		max = 255;
+		rand_byte = rand() % (max + 1); //Generates a random number between 0 to 255
+		kk = getKK();
+		x = getX();
+		v[x] = rand_byte & kk;
+		PC = PC + 2;
+		break;
+	}
+			   //verified
+	case 0xD000: {
+		//Dxyn - DRW Vx, Vy, nibble
+		//Display n - byte sprite starting at memory location I at(Vx, Vy), set VF = collision.
+		unsigned char x, y;
+		unsigned short height;
+		unsigned char pixel;
+
+		x = getX();
+		y = getY();
+		height = Opcode & 0x000F;
+		v[0xF] = 0; //0xF = Vf
+		for (int yaxis = 0; yaxis < height; yaxis++) {
+			pixel = Memory[I + yaxis];
+			for (int xaxis = 0; xaxis < 8; xaxis++) {
+				//xaxis < 8 because a byte has 8 bits
+
+				if ((pixel & (0x80 >> xaxis)) != 0) {
+					if (Graphics[(x + xaxis + (y + yaxis) * 64)] == 1) {
+						//Checking if the current pixel is set or not. If it is set, the collision is registered to VF
+						v[0xF] = 1;
+					}
+					Graphics[(x + xaxis + (y + yaxis * 64))] = Graphics[(x + xaxis + (y * yaxis * 64))] ^ 1;
+				}
+			}
+		}
+		drawFlag = true;
+		PC = PC + 2;
+
+		break;
+	}
+			   //verified
+	case 0xE000: {
+		unsigned short last;
+		unsigned char x;
+		last = Opcode & 0x00FF;
+		x = getX();
+
+		if (last == 0x009E) {
+			//Ex9E - SKP Vx
+			//Skip next instruction if key with the value of Vx is pressed.
+
+			if (Key[v[x]] != 0) {
+				PC = PC + 4;
+			}
+			else {
+				PC = PC + 2;
+			}
+		}
+		else if (last == 0x00A1) {
+			//ExA1 - SKNP Vx
+			//Skip next instruction if key with the value of Vx is not pressed.
+
+			if (Key[v[x]] == 0) {
+				PC = PC + 4;
+			}
+			else {
+				PC = PC + 2;
+			}
+		}
+		else {
+			cout << "\nUnsupported 0xE000 series Opcode\n";
+		}
+		break;
+	}
+			   //verified
+	case 0xF000: {
+		unsigned short last;
+		unsigned char x;
+		last = Opcode & 0x00FF;
+		x = getX();
+
+		switch (last) {
+		case 0x0007: {
+			//Fx07 - LD Vx, DT
+			//Set Vx = delay timer value.
+			v[x] = DelayTimer;
+			break;
+		}
+
+		case 0x000A: {
+			//Fx0A - LD Vx, K
+			//Wait for a key press, store the value of the key in Vx.
+
+			bool key_press = false;
+
+			for (int i = 0; i < 16; i++) {
+				if (Key[i] != 0) {
+					v[x] = i;
+					key_press = true;
+				}
+			}
+
+			if (!key_press) {
+				//Incase of no keypress, it returns and tries again
+				return;
+			}
+		}
+
+		case 0x0015: {
+			//Fx15 - LD DT, Vx
+			//Set delay timer = Vx.
+			DelayTimer = v[x];
+			break;
+		}
+
+		case 0x0018: {
+			//Fx18 - LD ST, Vx
+			//Set sound timer = Vx.
+			SoundTimer = v[x];
+			break;
+		}
+
+		case 0x001E: {
+			//Fx1E - ADD I, Vx
+			//Set I = I + Vx.
+			if (I + v[x] > 0xFFF) {
+				v[0xF] = 1;
+			}
+			else {
+				v[0xF] = 0;
+			}
+			I = I + v[x];
+			break;
+		}
+
+		case 0x0029: {
+			//Fx29 - LD F, Vx
+			//Set I = location of sprite for digit Vx.
+
+			I = v[x] * 0x5;
+			break;
+		}
+
+		case 0x0033: {
+			//Fx33 - LD B, Vx
+			//Store BCD representation of Vx in memory locations I, I + 1, and I + 2.
+
+			unsigned char digit_0s, digit_10s, digit_100s;
+			unsigned short value;
+
+			value = v[x];
+			//Obtaining the digits
+			digit_0s = value % 10;
+			value = value / 10;
+			digit_10s = value % 10;
+			value = value / 10;
+			digit_100s = value % 10;
+
+			//Storing in memory
+			Memory[I] = digit_100s;
+			Memory[I + 1] = digit_10s;
+			Memory[I + 2] = digit_0s;
+
+			//Shouldn't I be incremented? hmmm
+			break;
+		}
+
+		case 0x0055: {
+			//Fx55 - LD [I], Vx
+			//Store registers V0 through Vx in memory starting at location I.
+			for (int i = 0; i <= x; i++) {
+				Memory[I + i] = v[i];
+			}
+			I = x + I + 1;
+			break;
+		}
+
+		case 0x0065: {
+			//Fx65 - LD Vx, [I]
+			//Read registers V0 through Vx from memory starting at location I.
+			for (int i = 0; i <= x; i++) {
+				v[i] = Memory[I + i];
+			}
+			I = x + I + 1;
+			break;
+		}
+
+		default: {
+			printf("Unknown Opcode [0x0F00] : 0x%x\n", Opcode);
+		}
+
+		}
+		PC = PC + 2;
+		break;
+	}
+
+	default: {
+		printf("Unknown Opcode : 0x%X\n", Opcode);
+	}
+	}
+
+	if (DelayTimer > 0) {
+		DelayTimer--;
+	}
+
+	if (SoundTimer > 0) {
+		if (SoundTimer == 1) {
+			//Implement sound code
+		}
+
+		SoundTimer--;
+	}
+}
+unsigned char Entu::getX() {
+	/*
+	In all Chip 8 Opcode, if X exists, it is in bits 5-8
+	That is, 0x0X00
+	This function obtains the X value and returns it.
+	*/
+	unsigned char x;
+	unsigned char ret_x;
+
+	x = (Opcode & 0x0F00) >> 8;
+	ret_x = x;
+	return ret_x;
+}
+unsigned char Entu::getY() {
+	/*
+	In all Chip 8 Opcode, if X exists, it is in bits 9-12
+	That is, 0x00Y0
+	This function obtains the Y value and returns it.
+	*/
+	unsigned char y;
+	unsigned char ret_y;
+
+	y = Opcode & 0x00F0;
+	y = y >> 4;
+	ret_y = y;
+	return ret_y;
+}
+unsigned char Entu::getKK() {
+	/*
+	In all Chip 8 Opcode, if KK exists, it is in the second byte
+	That is, 0x00KK
+	This function obtains the K value and returns it.
+	*/
+	unsigned char kk;
+	unsigned char ret_kk;
+
+	kk = Opcode & 0x00FF;
+	ret_kk = kk;
+	return ret_kk;
+}
+unsigned short Entu::getOpcode(unsigned char a, unsigned char b) {
+	/*
+	Memory in Chip 8 is one byte
+	But opcodes are two bytes
+	In order to read one opcode, two memory bytes have to be read
+	This function combines the two memory bytes and returns a single 2 byte opcode
+
+	2 Bytes = 16 bits
+	The first memory byte is left-shifted by 8 bits. This adds 8 Zeroes to its left
+	A bitwise OR operation is then performed between the left-shifted memory byte and the second memory byte
+	The resultant provides us the actual opcode
+	*/
+	unsigned short code;
+
+	code = a << 8; // First Memory byte is left shifted 8 times
+	code = code | b; // Bitwise OR operation with the Second Memory byte
+	return code;
+}
